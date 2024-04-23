1ab84d3ba585579894ac6e2fe2c20ac1eab10820
Ankun <sytungan@gmail.com>
2024-04-24 01:20:01 +0700

    feat(profile): add posts page

--- lib/src/features/profile/presentation/widgets/sub_profiles/posts/sub_post_page.dart
+++ lib/src/features/profile/presentation/widgets/sub_profiles/posts/sub_post_page.dart
@@ -1,5 +1,10 @@
+import 'package:demo_dprofiles/src/core/app_responsive.dart';
+import 'package:demo_dprofiles/src/core/di/di.dart';
+import 'package:demo_dprofiles/src/features/home/presentation/bloc/home_bloc.dart';
+import 'package:demo_dprofiles/src/features/home/presentation/widgets/news_feed_home.dart';
 import 'package:demo_dprofiles/src/features/profile/data/models/user_info_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SubPostPage extends StatelessWidget {
   final UserInfoModel userInfo;
@@ -8,6 +13,12 @@ class SubPostPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: context.padding(top: 20),
+      child: BlocProvider.value(
+        value: injector.get<HomeBloc>(),
+        child: const NewsFeedHome(),
+      ),
+    );
   }
 }
